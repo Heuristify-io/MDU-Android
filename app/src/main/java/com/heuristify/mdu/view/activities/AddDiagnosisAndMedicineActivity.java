1053464177173ce9f0a844624f9b9d39dfd6bef4
luqman456 <muhammadluqmankhattak@gmail.com>
2021-03-20 17:12:18 +0500

    activity created for new diagonsis and medicine

---
+++ app/src/main/java/com/heuristify/mdu/view/activities/AddDiagnosisAndMedicineActivity.java
@@ -0,0 +1,16 @@
+package com.heuristify.mdu.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heuristify.mdu.R;
+
+public class AddDiagnosisAndMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_diagnosis_and_medicine);
+    }
+}
